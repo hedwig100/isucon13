5fcb7097046a489bff81436da669ae8a83923e56
hedwig100 <shota.in26@gmail.com>
2023-11-25 02:11:42 +0000

    [add] alp1110.log

--- perf-tools/alp.sh
+++ perf-tools/alp.sh
@@ -15,7 +15,7 @@ if [ $# -eq 1 ]; then
 fi
 
 # MEMO: -m 以降はグループ化して計測したいuriを書く
-sudo alp ltsv --file=/var/log/nginx/access.log --sort sum --reverse -q --nosave-pos --qs-ignore-values -m "/api/isu/,/register/,/isu/,/api/condition/"
+sudo alp ltsv --file=/var/log/nginx/access.log --sort sum --reverse -q --nosave-pos --qs-ignore-values -m "/api/user/,/api/livestream/"
 
 if [ -n "$rotate_filepath" ]; then
     sudo mv /var/log/nginx/access.log $rotate_filepath
