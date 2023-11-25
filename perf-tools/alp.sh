83b6eb209503f2c1a8e1136549dd7424910dd4b0
hedwig100 <shota.in26@gmail.com>
2023-11-25 02:43:34 +0000

    [add] alp log

--- perf-tools/alp.sh
+++ perf-tools/alp.sh
@@ -15,7 +15,7 @@ if [ $# -eq 1 ]; then
 fi
 
 # MEMO: -m 以降はグループ化して計測したいuriを書く
-sudo alp ltsv --file=/var/log/nginx/access.log --sort sum --reverse -q --nosave-pos --qs-ignore-values -m "/api/user/,/api/livestream/"
+sudo alp ltsv --file=/var/log/nginx/access.log --sort sum --reverse -q --nosave-pos --qs-ignore-values -m "/api/initialize,/api/tag./api/user/[0-9a-zA-Z]+/theme,/api/livestream/reservation,/api/livestream/search,/api/user/[0-9a-zA-Z]+/livestream,/api/livestream/[0-9]+/livecomment,/api/livestream/[0-9]+/reaction,/api/livestream/[0-9]+/report,/api/livestream/[0-9]+/ngwords,/api/livestream/[0-9]+/livecomment/[0-9]+/report,/api/livestream/[0-9]+/moderate,/api/livestream/[0-9]+/enter,/api/livestream/[0-9]+/exit,/api/user/[0-9a-zA-Z]+/statistics,/api/user/[0-9a-zA-Z]+/icon,/api/livestream/[0-9]+/statistics,/api/livestream/[0-9]+,/api/user/[0-9a-zA-Z]+"
 
 if [ -n "$rotate_filepath" ]; then
     sudo mv /var/log/nginx/access.log $rotate_filepath
